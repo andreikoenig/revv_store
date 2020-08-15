d3e205a14f5fd2a553371f735d1974b86d665efb
Andrei Koenig <nv1982@gmail.com>
2020-08-15 16:41:05 -0500

    add a model and migration for Product, add sample data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  { name: "Basic Package", description: "just the basic stuff", price: 2000 },
+  { name: "Gold Package", description: "Premium features and all the basic stuff", price: 5000 },
+  { name: "Diamond Package", description: "This includes everything!", price: 10000 }
+])
