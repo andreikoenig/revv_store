f9fee66687a95779f461d45706ac2fbf0b5bfb59
Andrei Koenig <nv1982@gmail.com>
2020-08-16 18:23:05 -0500

    get stripe checkout page, add pry gem

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,27 @@
 class OrdersController < ApplicationController
   def create
     @product = Product.find(params['product_id'])
+
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    puts Stripe.api_key
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: @product.name,
+            description: @product.description
+          },
+          unit_amount: @product.price,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: orders_success_url,
+      cancel_url: orders_cancel_url
+    )
   end
 
   def success
